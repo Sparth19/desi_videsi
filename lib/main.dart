e256561cafaee9d3df6487633180458739696d66
Sparth19 <parthshekhaliya18@gmail.com>
2020-06-25 11:42:20 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:desividesi/pages/MainPage.dart';
+import 'package:flutter/material.dart';
+
+void main()=>runApp(Myapp());
+
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:MainPage(),
+    );
+  }
+}
---
+++ lib/pages/MainPage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+      title: new Text("Desi Videsi"),
+      centerTitle: true,
+      elevation: 5,
+    ),
+      body: Container(
+
+      ),
+
+
+
+
+    );
+
+
+  }
+}
