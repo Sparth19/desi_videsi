ed09630da8c512fd96e7bf304fe02069658a59c6
royparth20 <royparth2094@gmail.com>
2020-06-29 14:29:40 +0530

    Category Screen Design change parth roy 2

--- lib/pages/Category_Page_1.dart
+++ lib/pages/Category_Page_1.dart
@@ -1,4 +1,3 @@
-import 'package:desividesi/color/MyTheme.dart';
 import 'package:desividesi/pages/HomePage.dart';
 import 'package:desividesi/service/models.dart';
 import 'package:flutter/material.dart';
@@ -8,25 +7,33 @@ class CategoryPageOne extends StatefulWidget {
   _CategoryPageOneState createState() => _CategoryPageOneState();
 }
 
+List x = new List();
+List showData = new List();
+
 List getCategoryData(category_name) {
-  List CategoryData = new List();
+  List categoryData = x;
+  List<String> temp = new List();
   for (MasterData x in Homepage.useMaster) {
 //      print(x.mainCategory);
 //      print(mainCategoryValues.map[category_name]);
     if (x.mainCategory == mainCategoryValues.map[category_name]) {
       print("name" + category_name);
-      CategoryData.add(x);
+      categoryData.add(x);
+      if (!temp.contains(x.category1)) {
+        showData.add(x);
+        temp.add(x.category1);
+      }
     }
   }
-  print("Length : " + CategoryData.length.toString());
-  return CategoryData;
+  print("Length : " + categoryData.length.toString());
+  return categoryData;
 }
 
 class _CategoryPageOneState extends State<CategoryPageOne> {
   @override
   Widget build(BuildContext context) {
     String check = ModalRoute.of(context).settings.arguments;
-    List x = getCategoryData(check);
+    x = getCategoryData(check);
 
     return Scaffold(
         appBar: AppBar(
@@ -35,12 +42,36 @@ class _CategoryPageOneState extends State<CategoryPageOne> {
             style: TextStyle(letterSpacing: 2, fontWeight: FontWeight.w700),
           ),
         ),
-        body: Text(
-          x.length.toString(),
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 28,
-              color: MyThemeColor.primaryTextColor),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: create_Category_List(),
+              ),
+            )
+          ],
         ));
+  }
+
+  Widget create_Category_List() {
+    return GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(showData.length, (int index) {
+          return InkWell(
+            child: Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width / 2,
+              child: Text("TExt"),
+            ),
+          );
+        }));
+    /*return new ListView.builder(
+        scrollDirection: Axis.vertical,
+        padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
+        itemCount: showData.length,
+        itemBuilder: (BuildContext context, int index) {
+
+        });*/
   }
 }
